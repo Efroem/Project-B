c9517173b44710ca4f94bc757dbc5f9a3e6ed696
manojlo14 <1073352@hr.nl>
2024-04-23 12:32:55 +0200

    Cimemazaal.cs de rijen goed gemaakt
    zoals titel zegt

--- Project B/CinemaZaal.cs
+++ Project B/CinemaZaal.cs
@@ -1,87 +1,140 @@
 public  class CinemaZaal
-{   /*
-    public static string GroteZaal = @"
-    ____________________________________________________
-    |  J [1] [2] [3] [4] [5] [6] [7] [8] [9] [10] [11]  |
-    |  I     [1] [2] [3] [4] [5] [6] [7] [8] [9]        | 
-    |  H [1] [2] [3] [4] [5] [6] [7] [8] [9] [10] [11]  |
-    |  G     [1] [2] [3] [4] [5] [6] [7] [8] [9]        |
-    |  F [1] [2] [3] [4] [5] [6] [7] [8] [9] [10] [11]  |
-    |  E     [1] [2] [3] [4] [5] [6] [7] [8] [9]        |
-    |  D [1] [2] [3] [4] [5] [6] [7] [8] [9] [10] [11]  |
-    |  C     [1] [2] [3] [4] [5] [6] [7] [8] [9]        |
-    |  B [1] [2] [3] [4] [5] [6] [7] [8] [9] [10] [11]  |
-    |  A     [1] [2] [3] [4] [5] [6] [7] [8] [9]        |
-    |                                                   |
-    |                                                   |
-    |                   filmdoek                        |
-    |___________________________________________________|
-    ";
+{   
+    private const string EmptyRowSeparator = "|                                                    |";
+	private const string EmptyRowSeparator2 = " |";
+	private const string EmptyRowSeparator3 = "         |";
+    private const string EmptyRowSeparator4 = "      |";
+    private const string EmptyRowSeparator5 = "|                                   |";
+	private const string EmptyRowSeparator6 = "        |";
+	private const string EmptyRowSeparator7 = "      |";
 
-    public static string MediumZaal = @"
-        ____________________________________________________
-        |  I     [1] [2] [3] [4] [5] [6] [7] [8] [9] [10]   |
-        |  H        [1] [2] [3] [4] [5] [6] [7] [8] [9]     |
-        |  G     [1] [2] [3] [4] [5] [6] [7] [8] [9] [10]   |
-        |  F        [1] [2] [3] [4] [5] [6] [7] [8] [9]     |
-        |  E     [1] [2] [3] [4] [5] [6] [7] [8] [9] [10]   |
-        |  H        [1] [2] [3] [4] [5] [6] [7] [8] [9]     |
-        |  G     [1] [2] [3] [4] [5] [6] [7] [8] [9] [10]   |
-        |  F        [1] [2] [3] [4] [5] [6] [7] [8] [9]     |
-        |  A     [1] [2] [3] [4] [5] [6] [7] [8] [9] [10]   |
-        |                                                   |
-        |                                                   |
-        |                   filmdoek                        |
-        |___________________________________________________|
-    ";
 
-    public static string KleineZaal = @"
-    _____________________________________
-    |  J   [1] [2] [3] [4] [5] [6]      |
-    |  I    [1] [2] [3] [4] [5]         |
-    |  H   [1] [2] [3] [4] [5] [6]      |
-    |  G    [1] [2] [3] [4] [5]         |
-    |  F   [1] [2] [3] [4] [5] [6]      |
-    |  E    [1] [2] [3] [4] [5]         |
-    |  D   [1] [2] [3] [4] [5] [6]      |
-    |  C    [1] [2] [3] [4] [5]         |
-    |  B   [1] [2] [3] [4] [5] [6]      |
-    |  A    [1] [2] [3] [4] [5]         |
-    |                                   |
-    |                                   |
-    |             filmdoek              |
-    |___________________________________|
-    ";
-    */
-     public void PrintGrid()
+    public void PrintGridGroteZaal()
     {
-        Console.WriteLine("____________________________________________________");
+        Console.WriteLine("_____________________________________________________");
+        Console.WriteLine("|												    |");
 
-        // Print the grid
+
         for (char c = 'A'; c <= 'J'; c++)
         {
-            Console.Write("|  " + c + " ");
-            for (int i = 1; i <= 11; i++)
+            Console.Write("|  " + c + "  ");
+            int maxSeats = (c - 'A') % 2 == 0 ? 11 : 9;
+            // hierboven staat een bereking van hoe het wordt berekent 
+            // (c - 'A') hier wordt met ascii table gebruikt gemaakt dus character - a.
+            // als c B is en B is 2 in ascii table dus het resutlaat zal dan 1 zijn etc.
+            // als (c - 'A') % 2 en even getal is zal er een row met 11 stoelen geprint worden en als het oneven is dan een row met 9 stoelen.
+            // zelfde is met de andere functies maar dan met andere getallen
+            for (int i = 1; i <= maxSeats; i++)
             {
-                if (i <= 9)
+                if (i == 1 && maxSeats == 9)
+                Console.Write("  ["+ i + "] ");
+				
+				else if (i <= 9)
                 {
-                    Console.Write("[ " + i + "] ");
+                    Console.Write("["+ i +"] ");
                 }
                 else
                 {
-                    Console.Write("[" + i + "] ");
+                    Console.Write("[" + i +  "] ");
                 }
             }
-            Console.WriteLine(" |");
+            if (maxSeats == 11)
+            {
+                Console.WriteLine(EmptyRowSeparator2);
+            }
+            else
+            {
+                Console.WriteLine(EmptyRowSeparator3);
+            }
             if (c != 'J')
             {
-                Console.WriteLine("|  " + new string(' ', 48) + "|");
+            Console.WriteLine(EmptyRowSeparator);
             }
         }
-        Console.WriteLine("|                                                   |");
-        Console.WriteLine("|                                                   |");
-        Console.WriteLine("|                   filmdoek                        |");
-        Console.WriteLine("|___________________________________________________|");
+
+        Console.WriteLine("|                                                    |");
+        Console.WriteLine("|                   filmdoek                         |");
+        Console.WriteLine("|____________________________________________________|");
+    }
+
+
+
+public void PrintGridMediumZaal()
+    {
+        Console.WriteLine("_____________________________________________________");
+        Console.WriteLine("|												    |");
+
+        for (char c = 'A'; c <= 'I'; c++)
+        {
+            Console.Write("|  " + c + "  ");
+            int maxSeats = (c - 'A') % 2 == 0 ? 10 : 9;
+            for (int i = 1; i <= maxSeats; i++)
+            {
+                if (i == 1 && maxSeats == 9)
+                Console.Write("  ["+ i + "] ");
+				
+				else if (i <= 9)
+                {
+                    Console.Write("["+ i +"] ");
+                }
+                else
+                {
+                    Console.Write("[" + i +  "] ");
+                }
+            }
+            if (maxSeats == 10)
+            {
+                Console.WriteLine(EmptyRowSeparator4);
+            }
+            else
+            {
+                Console.WriteLine(EmptyRowSeparator3);
+            }
+            if (c != 'J')
+            {
+            Console.WriteLine(EmptyRowSeparator);
+            }
+        }
+
+        // Print the bottom part
+        Console.WriteLine("|                                                    |");
+        Console.WriteLine("|                   filmdoek                         |");
+        Console.WriteLine("|____________________________________________________|");
+    }
+
+    public void PrintGridKleineZaal()
+    {
+        Console.WriteLine("____________________________________");
+        Console.WriteLine("|								   |");
+
+        for (char c = 'A'; c <= 'J'; c++)
+        {
+            Console.Write("|  " + c + "  ");
+            int maxSeats = (c - 'A') % 2 == 0 ? 6 : 5;
+            for (int i = 1; i <= maxSeats; i++)
+            {
+                if (i == 1 && maxSeats == 5)
+                Console.Write("  ["+ i + "] ");
+                else
+                Console.Write("[" + i + "] ");
+            }
+            if (maxSeats == 5)
+            {
+                Console.WriteLine(EmptyRowSeparator6);
+            }
+            else
+            {
+                Console.WriteLine(EmptyRowSeparator7);
+            }
+            if (c != 'J')
+            {
+            Console.WriteLine(EmptyRowSeparator5);
+            }
+        }
+
+        Console.WriteLine("|                                   |");
+        Console.WriteLine("|           filmdoek                |");
+        Console.WriteLine("|___________________________________|");
     }
 }
 
