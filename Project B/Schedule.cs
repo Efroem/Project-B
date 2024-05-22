f03ee5b9e31ed8b2b3edd6249da0397ebbbab969
john spruijt <johnspruijt@outlook.com>
2024-05-22 15:06:46 +0200

    Schema is nummer dynamisch en zaal naam staat er bij

--- Project B/Schedule.cs
+++ Project B/Schedule.cs
@@ -6,7 +6,11 @@ public class Schedule
     [JsonPropertyName("movieTitle")]
     public string JsonMovieTitle { set => MovieTitle = value; }
     [JsonPropertyName("serialNumber")]
-    public string JsonSerialNumber { set => SerialNumber = Convert.ToInt32(value); }
+    public string JsonSerialNumber { set {
+            SerialNumber = Convert.ToInt32(value);
+            Hall = CinemaHall.ReadFromCinemaHall().Find(x => x.SerialNumber == SerialNumber);
+        }
+    }
     [JsonPropertyName("date")]
     public string JsonDate { set => Date = DateTime.ParseExact(value, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture); }
 
@@ -14,6 +18,8 @@ public class Schedule
     public string MovieTitle { get; set; }
     public int SerialNumber { get; set; }
     public DateTime Date { get; set; }
+
+    public CinemaHall Hall {get; set;}
 
     public Schedule(string movieTitle, string serialNumber, string date) : this(movieTitle, Convert.ToInt32(serialNumber), DateTime.Parse(date))
     {
@@ -51,55 +57,76 @@ public class Schedule
         OpenScheduleMenu(schedules);
     }
 
-    public static void OpenSpecificMenu(string movieTitle) {
+    public static void OpenSpecificMenu(string movieTitle)
+    {
         List<Schedule> schedules = ReadScheduleJson();
         schedules = schedules.Where(x => x.MovieTitle == movieTitle).Where(x => x.Date > DateTime.Now).OrderBy(x => x.Date).ToList();
         OpenScheduleMenu(schedules);
     }
 
-    private static void OpenScheduleMenu(List<Schedule> schedules) {
+    private static void OpenScheduleMenu(List<Schedule> schedules)
+    {
         int currentIndex = 0;
         const int stepSize = 5;
 
-        while (true) {
+        while (true)
+        {
+            int previousIndex = 0;
             if (currentIndex != 0)
-                Console.WriteLine("1. Vorige");
-            for (int i = 0; i < Math.Min(stepSize, schedules.Count - currentIndex); i++)
             {
-                Console.WriteLine($"{i+2}. {schedules[currentIndex + i].MovieTitle}");
+                previousIndex = 1;
+                Console.WriteLine($"{previousIndex}. Vorige");
+            }
+
+            int moviesShownAmount = Math.Min(stepSize, schedules.Count - currentIndex);
+            for (int i = 0; i < moviesShownAmount; i++)
+            {
+                Console.WriteLine($"{i + 1 + previousIndex}. {schedules[currentIndex + i].MovieTitle}");
                 Console.WriteLine(schedules[currentIndex + i].Date);
+                Console.WriteLine(schedules[currentIndex + i].Hall.Name);
                 Console.WriteLine();
             }
+
+            string nextNumber;
+            string backNumber;
             if (currentIndex + 5 < schedules.Count)
-            Console.WriteLine("7. Volgende");
-            Console.WriteLine("8. Terug naar menu");
+            {
+                nextNumber = Convert.ToString(moviesShownAmount + 1 + previousIndex);
+                backNumber = Convert.ToString(moviesShownAmount + 2 + previousIndex);
+                Console.WriteLine($"{nextNumber}. Volgende");
+            }
+            else
+            {
+                nextNumber = "_";
+                backNumber = Convert.ToString(moviesShownAmount + 1 + previousIndex);
+            }
+
+            Console.WriteLine($"{backNumber}. Terug naar menu");
 
             string userAction = (Console.ReadLine() ?? "").ToLower();
-            switch (userAction)
-            {   
-                case "1":
-                case "vorige":
-                    currentIndex = currentIndex - 5 < 0 ? 0 : currentIndex - 5;
-                    break; 
-                case "7":
-                case "volgende":
-                    currentIndex = schedules.Count - currentIndex > stepSize 
-                        ? currentIndex + stepSize > schedules.Count - currentIndex 
-                            ? schedules.Count - stepSize 
-                            : currentIndex + stepSize 
+            if (userAction == "1" || userAction == "vorige")
+            {
+                currentIndex = currentIndex - 5 < 0 ? 0 : currentIndex - 5;
+            }
+            else if (userAction == nextNumber || userAction == "volgende")
+            {
+                currentIndex = schedules.Count - currentIndex > stepSize
+                        ? currentIndex + stepSize > schedules.Count - currentIndex
+                            ? schedules.Count - stepSize
+                            : currentIndex + stepSize
                         : currentIndex;
-                    break;
-                case "8":
-                case "terug":
-                case "terug naar menu":
-                case "menu":
-                    return;
+            }
+            else if (userAction == backNumber || userAction.Contains("terug") || userAction.Contains("menu"))
+            {
+                return;
             }
 
             for (int i = 0; i < Math.Min(stepSize, schedules.Count - currentIndex); i++)
             {
-                if (userAction == Convert.ToString(i+2) || userAction == schedules[currentIndex + i].MovieTitle) {
-                    Console.WriteLine($"You picked {i+2}. {schedules[currentIndex + i].MovieTitle}");
+                if (userAction == Convert.ToString(i + 1 + previousIndex) || userAction == schedules[currentIndex + i].MovieTitle)
+                {
+                    Console.WriteLine($"You picked {i + 1 + previousIndex}. {schedules[currentIndex + i].MovieTitle}");
+                    Console.ReadLine();
                 }
             }
 
