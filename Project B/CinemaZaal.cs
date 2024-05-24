d37e52ce500c4c12436590e77ff6cb8452578992
manojlo14 <1073352@hr.nl>
2024-05-24 13:22:58 +0200

    pijltjes gefixt voor de stoelen selecteren
    pijltjes gefixt voor de stoelen selecteren

--- Project B/CinemaZaal.cs
+++ Project B/CinemaZaal.cs
@@ -7,16 +7,16 @@ public  class CinemaZaal
     private const string EmptyRowSeparator5 = "|                                   |";
 	private const string EmptyRowSeparator6 = "        |";
 	private const string EmptyRowSeparator7 = "      |";
-
+    private HashSet<int> executedRows = new HashSet<int>();
     private int currentRow = 2; 
     private int cursorPosition = 5;
-
     public void NavigateGrid()
     {
         while (true)
-        {
+        {   
+            SetInitialCursorPosition();
+            
             Console.SetCursorPosition(cursorPosition, currentRow);
-
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 
             if (keyInfo.Key == ConsoleKey.UpArrow && currentRow > 1)
@@ -27,13 +27,31 @@ public  class CinemaZaal
             {
                 currentRow += 2;
             }
-            else if (keyInfo.Key == ConsoleKey.LeftArrow && cursorPosition > 5)
+            else if (keyInfo.Key == ConsoleKey.LeftArrow )// && cursorPosition > 4)
             {
-                cursorPosition -= 3;
+                if(currentRow == 4 ||currentRow == 8 || currentRow == 10)
+                {
+                    cursorPosition -= 6;
+                }
+                else
+                {
+                    cursorPosition -= 4;
+                }
             }
-            else if (keyInfo.Key == ConsoleKey.RightArrow && cursorPosition < 45)
+            // else if (keyInfo.Key == ConsoleKey.LeftArrow|| currentRow == 4 ||currentRow == 8 || currentRow == 10)// && cursorPosition > 4)
+            // {
+            //     cursorPosition -= 6;
+            // }
+            else if (keyInfo.Key == ConsoleKey.RightArrow )//&& cursorPosition < 45)
             {
-                cursorPosition += 3;
+                if(currentRow == 4 ||currentRow == 8 || currentRow == 10)
+                {
+                    cursorPosition +=2 ;
+                }
+                else
+                {
+                    cursorPosition += 4;
+                }
             }
             else if (keyInfo.Key == ConsoleKey.Escape)
             {
@@ -42,7 +60,34 @@ public  class CinemaZaal
         }
     }
 
+    // /*
+    // private void SetInitialCursorPosition()
+    // {
+    //     if(!isCursorPositionSet)
+    //         {
+    //         if (currentRow == 4 ||currentRow == 8 || currentRow == 10 )
+    //             {
+    //                 cursorPosition = cursorPosition + 2;
+    //                 isCursorPositionSet = true;
 
+    //             }
+    //         }
+    // }
+    // */
+    //currentRow % 2 == 1
+     private void SetInitialCursorPosition()
+    {
+        
+        if (currentRow == 4 ||currentRow == 8 || currentRow == 10|| currentRow == 14 || currentRow == 18 || currentRow == 20)
+        {
+            cursorPosition = cursorPosition + 2;
+        }
+        else
+        {
+            cursorPosition = cursorPosition - 2;
+        }
+
+    }
     public void PrintGridGroteZaal()
     {
         Console.WriteLine("_____________________________________________________");
@@ -51,16 +96,16 @@ public  class CinemaZaal
 
         for (char c = 'A'; c <= 'J'; c++)
         {
-            Console.Write("|  " + c + "  ");
-            int maxSeats = (c - 'A') % 2 == 0 ? 11 : 9;
+            Console.Write("|  " + c + "");
+            GlobalVariables.maxSeats1 = (c - 'A') % 2 == 0 ? 11 : 9;            
             // hierboven staat een bereking van hoe het wordt berekent 
             // (c - 'A') hier wordt met ascii table gebruikt gemaakt dus character - a.
             // als c B is en B is 2 in ascii table dus het resutlaat zal dan 1 zijn etc.
             // als (c - 'A') % 2 en even getal is zal er een row met 11 stoelen geprint worden en als het oneven is dan een row met 9 stoelen.
             // zelfde is met de andere functies maar dan met andere getallen
-            for (int i = 1; i <= maxSeats; i++)
+            for (int i = 1; i <= GlobalVariables.maxSeats1; i++)
             {
-                if (i == 1 && maxSeats == 9)
+                if (i == 1 && GlobalVariables.maxSeats1 == 9)
                 Console.Write("  ["+ i + "] ");
 				
 				else if (i <= 9)
@@ -72,7 +117,7 @@ public  class CinemaZaal
                     Console.Write("[" + i +  "] ");
                 }
             }
-            if (maxSeats == 11)
+            if (GlobalVariables.maxSeats1 == 11)
             {
                 Console.WriteLine(EmptyRowSeparator2);
             }
@@ -101,10 +146,10 @@ public void PrintGridMediumZaal()
         for (char c = 'A'; c <= 'I'; c++)
         {
             Console.Write("|  " + c + "");
-            int maxSeats = (c - 'A') % 2 == 0 ? 10 : 9;
-            for (int i = 1; i <= maxSeats; i++)
+            GlobalVariables.maxSeats2 = (c - 'A') % 2 == 0 ? 10 : 9;
+            for (int i = 1; i <= GlobalVariables.maxSeats2; i++)
             {
-                if (i == 1 && maxSeats == 9)
+                if (i == 1 && GlobalVariables.maxSeats2 == 9)
                 Console.Write("  ["+ i + "] ");
 				
 				else if (i <= 9)
@@ -116,7 +161,7 @@ public void PrintGridMediumZaal()
                     Console.Write("[" + i +  "] ");
                 }
             }
-            if (maxSeats == 10)
+            if (GlobalVariables.maxSeats2 == 10)
             {
                 Console.WriteLine(EmptyRowSeparator4);
             }
@@ -144,15 +189,15 @@ public void PrintGridMediumZaal()
         for (char c = 'A'; c <= 'J'; c++)
         {
             Console.Write("|  " + c + "  ");
-            int maxSeats = (c - 'A') % 2 == 0 ? 6 : 5;
-            for (int i = 1; i <= maxSeats; i++)
+            GlobalVariables.maxSeats3 = (c - 'A') % 2 == 0 ? 6 : 5;
+            for (int i = 1; i <= GlobalVariables.maxSeats3; i++)
             {
-                if (i == 1 && maxSeats == 5)
+                if (i == 1 && GlobalVariables.maxSeats3 == 5)
                 Console.Write("  ["+ i + "] ");
                 else
                 Console.Write("[" + i + "] ");
             }
-            if (maxSeats == 5)
+            if (GlobalVariables.maxSeats3 == 5)
             {
                 Console.WriteLine(EmptyRowSeparator6);
             }
---
+++ Project B/GlobalVariables.cs
@@ -0,0 +1,6 @@
+public static class GlobalVariables
+{
+    public static int maxSeats1;
+    public static int maxSeats2;
+    public static int maxSeats3;
+}
