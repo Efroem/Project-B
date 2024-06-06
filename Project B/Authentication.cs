5cb2b2e36badc85e7db455dcb4977d1fa91d4d40
Efroem <ejhoekjr@gmail.com>
2024-06-06 14:46:00 +0200

    Console.Clear()

--- Project B/Authentication.cs
+++ Project B/Authentication.cs
@@ -300,7 +300,11 @@ static class Authentication
                 Console.WriteLine("Bevestig wachtwoord:");
                 string passwordConfirm = ReadPassword();
                 if (password != passwordConfirm)
+                {
+                    Console.Clear();
                     throw new Exception("Wachtwoorden komen niet overeen");
+                }
+
                 else
                     return password;
             }
